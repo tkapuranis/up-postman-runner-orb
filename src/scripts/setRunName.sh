b9cb15c76f4a39423e5f11b8ba8d35237fb46d5e
tkapuranis <thanasi.kapuranis@uplight.com>
2023-06-05 17:08:31 -0600

    .

--- src/scripts/setRunName.sh
+++ src/scripts/setRunName.sh
@@ -3,4 +3,4 @@ DATENOW=$( date '+%F_%H:%M:%S' )
 TITLE="$CIRCLE_PROJECT_REPONAME $CIRCLE_BRANCH $DATENOW"
 echo "$TITLE"
 echo export TESTRAIL_TITLE="${TITLE}" >> "$BASH_ENV"
-echo "$BASH_ENV"
+source "$BASH_ENV"
