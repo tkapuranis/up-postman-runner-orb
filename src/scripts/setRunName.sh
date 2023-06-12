60f7e1fb7eeb85ba9774618cb0fd8189ab7a4894
tkapuranis <thanasi.kapuranis@uplight.com>
2023-06-12 09:49:09 -0600

    param change

--- src/scripts/setRunName.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# shellcheck source=/dev/null
-DATENOW=$( date '+%F_%H:%M:%S' )
-TITLE="$CIRCLE_PROJECT_REPONAME $CIRCLE_BRANCH $DATENOW"
-echo "$TITLE"
-echo export TESTRAIL_TITLE="${TITLE}" >> "$BASH_ENV"
