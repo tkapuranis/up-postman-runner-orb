67d5399531e6a12868bff2c2d7d4b2ba4acd4a10
tkapuranis <thanasi.kapuranis@uplight.com>
2023-06-12 12:38:56 -0600

    run name

---
+++ src/scripts/installReporter.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+npm install newman-reporter-testrail@1.0.52 --global
+TESTRAIL_RUN_NAME="$CIRCLE_PROJECT_REPONAME $CIRCLE_BRANCH $DATENOW"
+echo "$TESTRAIL_RUN_NAME"
+echo export TESTRAIL_TITLE="${TESTRAIL_RUN_NAME}" >> "$BASH_ENV"
+source "$BASH_ENV"
+echo "$TESTRAIL_TITLE"
